9a1e554d866e5d7f737242e07af4feb90febbd6c
Yousef <yousefsaleh20@icloud.com>
2022-07-29 18:18:28 -0500

    commit tree fbd8d9808a419bf66d0e9962657a853c20873bec

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: 'yousef',
+    streetAdress: '123 lane',
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = {...obj}
+    newEmployee[key] = value;
+
+    return newEmployee;
+  };
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key,) {
+    const employeeTwo = {...employee}
+    
+    delete employeeTwo[key];
+    return employeeTwo;
+  }
+
+  function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+  }
+
